02964770deca6bafd85e86983e5debbc285bc05a
leon <90852553+leon0719@users.noreply.github.com>
2023-12-29 21:18:15 +0800

    📦 NEW: update

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api', views.getRoutes, name='routes'),
-    path('api/products/', views.getProducts, name='products'),
-    path('api/product/<str:pk>/', views.getProduct, name='product'),
+    path("api", views.getRoutes, name="routes"),
+    path("api/products/", views.getProducts, name="products"),
+    path("api/product/<str:pk>/", views.getProduct, name="product"),
+    path("", views.hello, name="hello"),
 ]
--- backend/base/views.py
+++ backend/base/views.py
@@ -2,25 +2,31 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .products import products
+
 # Create your views here.
 
 
-@api_view(['GET'])
+@api_view(["GET"])
 def getRoutes(request):
-    routes = ['/api/products', '/api/products/create/']
+    routes = ["/api/products", "/api/products/create/"]
     return Response(routes)
 
 
-@api_view(['GET'])
+@api_view(["GET"])
 def getProducts(request):
     return Response(products)
 
 
-@api_view(['GET'])
+@api_view(["GET"])
 def getProduct(request, pk):
     product = None
     for i in products:
-        if i['_id'] == pk:
+        if i["_id"] == pk:
             product = i
             break
     return Response(product)
+
+
+@api_view(["GET"])
+def hello(request):
+    return Response("Hello Django")

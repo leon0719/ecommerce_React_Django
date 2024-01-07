50166a2a51eb0d36c6c6c3c993eb76bb38750343
leon <90852553+leon0719@users.noreply.github.com>
2024-01-07 11:05:06 +0800

    🐛 FIX: bug

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -8,11 +8,14 @@ export const productListReducer = (state = { products: [] }, action) => {
   switch (action.type) {
     case PRODUCT_LIST_REQUEST:
       return { loading: true, products: [] };
+
     case PRODUCT_LIST_SUCCESS:
       return { loading: false, products: action.payload };
+
     case PRODUCT_LIST_FAIL:
-      return { loading: false, products: action.payload };
+      return { loading: false, error: action.payload };
+
     default:
-      return state;
+      return state
   }
-};
+}

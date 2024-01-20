85d41dff324083f9b13cc0a17062e6162810850e
leon <90852553+leon0719@users.noreply.github.com>
2024-01-20 12:07:33 +0800

    📦 NEW: Create productReducer

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,10 +1,11 @@
 import { legacy_createStore as createStore, applyMiddleware, combineReducers } from 'redux';
 import {thunk} from 'redux-thunk';
 import {composeWithDevTools} from 'redux-devtools-extension';
-import { productListReducer } from './reducers/productReducers';
+import { productListReducer, productDetailReducer } from './reducers/productReducers';
 
 const reducer = combineReducers({
     productList: productListReducer,
+    productDetails: productDetailReducer,
 })
 
 const initialState = {}

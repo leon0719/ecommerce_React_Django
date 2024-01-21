cbb69787a39cbbe55ddfc9dab68a54e944445f38
leon <90852553+leon0719@users.noreply.github.com>
2024-01-21 10:46:59 +0800

    📦 NEW: create Cart Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import Footer from "./components/Footer";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
-
+import CartScreen from "./screens/CartScreen";
 function App() {
   return (
     <Router>
@@ -14,6 +14,7 @@ function App() {
         <Routes>
         <Route path='/' element={<HomeScreen></HomeScreen>} />
         <Route path='/product/:id' element={<ProductScreen></ProductScreen>} />
+        <Route path='/cart/:id' element={<CartScreen></CartScreen>} />
         </Routes>
         </Container>
       </main>

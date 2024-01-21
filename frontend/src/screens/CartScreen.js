31bca1caec613e51c2df340edfe43e8893be63ea
leon <90852553+leon0719@users.noreply.github.com>
2024-01-21 12:09:25 +0800

    📦 NEW: update

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,6 +1,37 @@
 import React, { useEffect } from "react";
-import { Link } from "react-router-dom";
-function CartScreen() {
+import {
+  Link,
+  useParams,
+  useNavigate,
+  useSearchParams,
+} from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  Row,
+  Col,
+  ListGroup,
+  Image,
+  Form,
+  Button,
+  Card,
+} from "react-bootstrap";
+import Message from "../components/Message";
+import { addToCart } from "../actions/cartActions";
+
+function CartScreen(Location) {
+  const productId = useParams();
+  const [searchqty, setSearchqty] = useSearchParams();
+  const qty = Number(searchqty.get("qty")) ? Number(searchqty.get("qty")) : 1;
+  const dispatch = useDispatch();
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+  console.log("cartItems", cartItems);
+  useEffect(() => {
+    if (productId) {
+      dispatch(addToCart(productId.id, qty));
+    }
+  }, [dispatch, productId, qty]);
+
   return <div>Cart</div>;
 }
 

0fd93e9ffa7c2b285b9a19124313c06eee2d6322
leon <90852553+leon0719@users.noreply.github.com>
2024-01-21 11:25:44 +0800

    📦 NEW: update

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,9 +1,7 @@
-import React from 'react'
-
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
 function CartScreen() {
-  return (
-    <div>Cart</div>
-  )
+  return <div>Cart</div>;
 }
 
-export default CartScreen
+export default CartScreen;

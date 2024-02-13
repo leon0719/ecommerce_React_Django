4aa6f1308208d7e041da3677d2df91780c9746bd
leon <90852553+leon0719@users.noreply.github.com>
2024-02-13 18:58:03 +0800

    📦 NEW: Create swagger API Document

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -18,10 +18,28 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view as swagger_get_schema_view
+
+
+schema_view = swagger_get_schema_view(
+    openapi.Info(
+        title="Ecommerce API",
+        default_version="v1",
+        description="Ecommerce API Documentation",
+    ),
+    public=True,
+)
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("base.urls")),
+    path(
+        "swagger/schema/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

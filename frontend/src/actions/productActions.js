50f46670ed43030186123d2c95ea2ea196ceb0b7
leon <90852553+leon0719@users.noreply.github.com>
2024-03-01 15:32:07 +0800

    update

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -12,7 +12,7 @@ import {
 export const listProducts = () => async (dispatch) => {
   try {
     dispatch({ type: PRODUCT_LIST_REQUEST });
-    const { data } = await axios.get("/api/products/");
+    const { data } = await axios.get("/api/products");
     dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data });
   } catch (error) {
     dispatch({
@@ -29,7 +29,7 @@ export const listProducts = () => async (dispatch) => {
 export const listProductDetails = (id) => async (dispatch) => {
   try {
     dispatch({ type: PRODUCT_DETAILS_REQUEST });
-    const { data } = await axios.get(`/api/product/${id}/`);
+    const { data } = await axios.get(`/api/product/${id}`);
     dispatch({ 
       type: PRODUCT_DETAILS_SUCCESS, 
       payload: data 

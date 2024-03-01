ebcbcd5dcff7829a3661c34c8c3e16a0084c893a
leon <90852553+leon0719@users.noreply.github.com>
2024-03-01 17:02:07 +0800

    update

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -24,8 +24,8 @@ export const removeFromCart = (id) => (dispatch, getState) => {
   dispatch({
     type: CART_REMOVE_ITEM,
     payload: id,
-  });
+  })
 
-  localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
+  localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems))
   
 }

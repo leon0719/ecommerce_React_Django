5afcfda805faf79f709ba9c8c1e6d19374826477
leon <90852553+leon0719@users.noreply.github.com>
2024-03-02 12:12:31 +0800

    📦 NEW: Add JWT token

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 from . import views
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-)
-
 
 urlpatterns = [
-    path("api/users/login", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path(
+        "api/users/login",
+        views.MyTokenObtainPairView.as_view(),
+        name="token_obtain_pair",
+    ),
     path("api", views.getRoutes, name="routes"),
+    path("api/users/profile", views.getUserProfile, name="users-profile"),
     path("api/products", views.getProducts, name="products"),
     path("api/product/<str:pk>", views.getProduct, name="product"),
     path("", views.hello, name="hello"),
--- backend/base/views.py
+++ backend/base/views.py
@@ -1,16 +1,52 @@
+from typing import Any, Dict
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Product
-from .serializer import ProductSerializer
+from .serializer import ProductSerializer, UserSerializer
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 # Create your views here.
 
 
+# 用於設定 payload 資訊
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        serializer = UserSerializer(self.user).data
+        for k, v in serializer.items():
+            data[k] = v
+
+        return data
+
+
+# 用於轉成 Json 格式
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
+
 @api_view(["GET"])
 def getRoutes(request):
-    routes = ["/api/products", "/api/products/create/"]
+    routes = [
+        "/api/products",
+        "/api/products/create/",
+        "/api/products/upload/",
+        "/api/products/<id>/reviews/",
+        "/api/products/top/",
+        "/api/products/<id>/",
+        "/api/products/delete/<id>/",
+        "/api/products/<update>/<id>",
+    ]
     return Response(routes)
+
+
+@api_view(["GET"])
+def getUserProfile(request):
+    user = request.user
+    serializer = UserSerializer(user, many=False)
+    return Response(serializer.data)
 
 
 @api_view(["GET"])

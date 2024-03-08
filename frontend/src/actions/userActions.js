6fe2afd83b055db8a9fe9910b25bda6fc74da379
leon <90852553+leon0719@users.noreply.github.com>
2024-03-08 13:07:19 +0800

    創建user action reducer

---
+++ frontend/src/actions/userActions.js
@@ -0,0 +1,44 @@
+import axios from "axios";
+import {
+  USER_LOGIN_REQUEST,
+  USER_LOGIN_SUSSESS,
+  USER_LOGIN_FAIL,
+  USER_LOGOUT,
+} from "../constants/userConstants";
+
+export const login = (email, password) => async (dispatch) => {
+  try {
+    dispatch({
+      type: USER_LOGIN_REQUEST,
+    });
+
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    const { data } = await axios.post(
+      "/api/users/login/",
+      {
+        username: email,
+        password: password,
+      },
+      config
+    );
+    dispatch({
+      type: USER_LOGIN_SUSSESS,
+      payload: data,
+    });
+    localStorage.setItem("userInfo", JSON.stringify(data));
+    
+  } catch {
+    dispatch({
+      type: USER_LOGIN_FAIL,
+      payload:
+        error.response && error.response.data.detail
+          ? error.response.data.detail
+          : error.message,
+    });
+  }
+};
---
+++ frontend/src/reducers/userReducers.js
@@ -0,0 +1,25 @@
+import {
+  USER_LOGIN_REQUEST,
+  USER_LOGIN_SUSSESS,
+  USER_LOGIN_FAIL,
+  USER_LOGOUT,
+} from "../constants/userConstants";
+
+export const userLoginReducer = (state = {}, action) => {
+  switch (action.type) {
+    case USER_LOGIN_REQUEST:
+      return { loading: true };
+
+    case USER_LOGIN_SUSSESS:
+      return { loading: false, products: action.payload };
+
+    case USER_LOGIN_FAIL:
+      return { loading: false, error: action.payload };
+
+    case USER_LOGOUT:
+      return {};
+
+    default:
+      return state;
+  }
+};

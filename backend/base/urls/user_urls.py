addd48845c3f2b2c2d6234445375138401fd5b1f
leon <90852553+leon0719@users.noreply.github.com>
2024-03-09 13:32:50 +0800

    👌 IMPROVE:  修改route

--- backend/base/urls/user_urls.py
+++ backend/base/urls/user_urls.py
@@ -4,11 +4,11 @@ from base.views import user_views as views
 
 urlpatterns = [
     path(
-        "login",
+        "login/",
         views.MyTokenObtainPairView.as_view(),
         name="token_obtain_pair",
     ),
-    path("register", views.registerUser, name="register"),
-    path("profile", views.getUserProfile, name="users-profile"),
+    path("register/", views.registerUser, name="register"),
+    path("profile/", views.getUserProfile, name="users-profile"),
     path("", views.getUsers, name="users"),
 ]

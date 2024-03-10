51d3ff7f0596b2b82d9dd4d64498df4161f30ce4
leon <90852553+leon0719@users.noreply.github.com>
2024-03-10 12:55:23 +0800

    創建user register screen action reducer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,8 @@ import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
 import LoginScreen from "./screens/LoginScreen";
+import RegisterScreen from "./screens/RegisterScreen";
+
 function App() {
   return (
     <Router>
@@ -15,6 +17,7 @@ function App() {
         <Routes>
         <Route path='/' element={<HomeScreen></HomeScreen>}/>
         <Route path='/login' element={<LoginScreen></LoginScreen>} />
+        <Route path='/register' element={<RegisterScreen></RegisterScreen>} />
         <Route path='/product/:id' element={<ProductScreen></ProductScreen>} />
         <Route path='/cart/:id' element={<CartScreen></CartScreen>} />
         </Routes>

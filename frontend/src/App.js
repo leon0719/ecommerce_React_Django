6532cb78dbb381baf9f2ed5024c84cc587b6b7b9
leon <90852553+leon0719@users.noreply.github.com>
2024-03-15 14:02:36 +0800

    創建 payment screen action reducer constant

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from "./screens/RegisterScreen";
 import ProfileScreen from "./screens/ProfileScreen";
 import ShippingScreen from "./screens/ShippingScreen";
+import PaymentScreen from "./screens/PaymentScreen";
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
         <Route path='/register' element={<RegisterScreen></RegisterScreen>} />
         <Route path='/profile' element={<ProfileScreen></ProfileScreen>} />
         <Route path='/shipping' element={<ShippingScreen></ShippingScreen>} />
+        <Route path='/payment' element={<PaymentScreen></PaymentScreen>} />
         <Route path='/product/:id' element={<ProductScreen></ProductScreen>} />
         <Route path='/cart/:id' element={<CartScreen></CartScreen>} />
         </Routes>

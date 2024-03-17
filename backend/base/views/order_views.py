1ab299cb98865d6c5170228458b0ce41445f1076
leon <90852553+leon0719@users.noreply.github.com>
2024-03-17 19:30:44 +0800

    fix bug

--- backend/base/views/order_views.py
+++ backend/base/views/order_views.py
@@ -5,7 +5,7 @@ from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 
 from base.models import Product, Order, OrderItem, ShippingAddress
-from backend.base.serializers import ProductSerializer
+from base.serializers import ProductSerializer
 
 from rest_framework import status
 from datetime import datetime

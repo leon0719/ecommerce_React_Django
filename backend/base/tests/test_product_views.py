f20fee3357f3129b6a484d38a19b79d736525468
leon <90852553+leon0719@users.noreply.github.com>
2024-03-25 13:23:36 +0800

    update test

--- backend/base/tests/test_product_views.py
+++ backend/base/tests/test_product_views.py
@@ -2,13 +2,7 @@ from django import urls
 import pytest
 
 
-@pytest.mark.parametrize(
-    "param",
-    [
-        ("products"),  # 測試獲取所有產品的路由
-    ],
-)
 @pytest.mark.django_db
-def test_product_views(client, param):
+def test_product_views(client):
     url = urls.reverse("products")  # 獲取獲取所有產品的 URL
     assert client.get(url).status_code == 200

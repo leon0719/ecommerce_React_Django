9ec047dc5ad46be166ee3f0848d926a258468307
leon <90852553+leon0719@users.noreply.github.com>
2024-03-27 21:04:40 +0800

    創建 deleteProduct API endpoint

--- backend/base/views/product_views.py
+++ backend/base/views/product_views.py
@@ -2,6 +2,8 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from base.models import Product
 from base.serializers import ProductSerializer
+from rest_framework.decorators import permission_classes
+from rest_framework.permissions import IsAdminUser
 
 
 @api_view(["GET"])
@@ -16,6 +18,14 @@ def getProduct(request, pk):
     product = Product.objects.get(_id=pk)  # SELECT * FROM product WHERE id = pk;
     serializer = ProductSerializer(product, many=False)
     return Response(serializer.data)
+
+
+@api_view(["DELETE"])
+@permission_classes([IsAdminUser])  # only admin can delete product
+def deleteProduct(request, pk):
+    product = Product.objects.get(_id=pk)  # SELECT * FROM product WHERE id = pk;
+    product.delete()
+    return Response("Producte Deleted")
 
 
 @api_view(["GET"])

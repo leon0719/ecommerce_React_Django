7e5f7312e40accc4200d4216889d03a46926fc47
leon <90852553+leon0719@users.noreply.github.com>
2024-03-31 12:47:21 +0800

    創建 updateProduct 和 createProduct API endpoint

--- backend/base/views/product_views.py
+++ backend/base/views/product_views.py
@@ -20,6 +20,42 @@ def getProduct(request, pk):
     return Response(serializer.data)
 
 
+@api_view(["POST"])
+@permission_classes([IsAdminUser])
+def createProduct(request):
+    user = request.user
+    product = Product.objects.create(
+        user=user,
+        name="Sample Name",
+        price=0,
+        brand="Sample Brand",
+        countInStock=0,
+        category="Sample Category",
+        description="",
+    )
+    serializer = ProductSerializer(product, many=False)
+    return Response(serializer.data)
+
+
+@api_view(["PUT"])
+@permission_classes([IsAdminUser])
+def updateProduct(request, pk):
+    data = request.data
+    product = Product.objects.get(_id=pk)  # SELECT * FROM product WHERE id = pk;
+
+    product.name = data["name"]
+    product.price = data["price"]
+    product.brand = data["brand"]
+    product.countInStock = data["countInStock"]
+    product.category = data["category"]
+    product.description = data["description"]
+
+    product.save()  # UPDATE product SET ...;
+
+    serializer = ProductSerializer(product, many=False)
+    return Response(serializer.data)
+
+
 @api_view(["DELETE"])
 @permission_classes([IsAdminUser])  # only admin can delete product
 def deleteProduct(request, pk):

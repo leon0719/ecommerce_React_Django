447f46502466b0ca663e25492e83304ebca9c923
leon <90852553+leon0719@users.noreply.github.com>
2024-04-04 17:23:48 +0800

    創建 product edit action constant reducer

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -3,18 +3,18 @@ import {
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
   PRODUCT_LIST_FAIL,
-
   PRODUCT_DETAILS_REQUEST,
   PRODUCT_DETAILS_SUCCESS,
   PRODUCT_DETAILS_FAIL,
-
   PRODUCT_DELETE_REQUEST,
   PRODUCT_DELETE_SUCCESS,
   PRODUCT_DELETE_FAIL,
-
   PRODUCT_CREATE_REQUEST,
   PRODUCT_CREATE_SUCCESS,
   PRODUCT_CREATE_FAIL,
+  PRODUCT_UPDATE_REQUEST,
+  PRODUCT_UPDATE_SUCCESS,
+  PRODUCT_UPDATE_FAIL,
 } from "../constants/productConstants";
 
 export const listProducts = () => async (dispatch) => {
@@ -85,7 +85,6 @@ export const deleteProduct = (id) => async (dispatch, getState) => {
   }
 };
 
-
 export const createProduct = () => async (dispatch, getState) => {
   try {
     dispatch({
@@ -103,10 +102,7 @@ export const createProduct = () => async (dispatch, getState) => {
       },
     };
 
-    const { data } = await axios.post(`/api/products/create/`,
-    {},
-    config
-  );
+    const { data } = await axios.post(`/api/products/create/`, {}, config);
 
     dispatch({
       type: PRODUCT_CREATE_SUCCESS,
@@ -115,6 +111,47 @@ export const createProduct = () => async (dispatch, getState) => {
   } catch (error) {
     dispatch({
       type: PRODUCT_CREATE_FAIL,
+      payload:
+        error.response && error.response.data.detail
+          ? error.response.data.detail
+          : error.message,
+    });
+  }
+};
+
+export const updateProduct = (product) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: PRODUCT_UPDATE_REQUEST,
+    });
+
+    const {
+      userLogin: { userInfo },
+    } = getState();
+
+    const config = {
+      headers: {
+        "Content-type": "application/json",
+        Authorization: `Bearer ${userInfo.access}`,
+      },
+    };
+
+    const { data } = await axios.put(
+      `/api/products/update/${product._id}/`,
+      product,
+      config
+    );
+
+    dispatch({
+      type: PRODUCT_UPDATE_SUCCESS,
+      payload: data, // data 是返回的 product 對象，而不是 product id
+    });
+
+    // 不過這邊不需要 dispatch PRODUCT_DETAILS_SUCCESS，因為更新後，product details 不會改變
+    dispatch({ type: PRODUCT_DETAILS_SUCCESS, payload: data });
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_UPDATE_FAIL,
       payload:
         error.response && error.response.data.detail
           ? error.response.data.detail
--- frontend/src/screens/ProductEditScreen.js
+++ frontend/src/screens/ProductEditScreen.js
@@ -10,7 +10,8 @@ import { useDispatch, useSelector } from "react-redux";
 import Loader from "../components/Loader";
 import Message from "../components/Message";
 import FormContainer from "../components/FormContainer";
-import { listProductDetails } from "../actions/productActions";
+import { listProductDetails, updateProduct } from "../actions/productActions";
+import { PRODUCT_UPDATE_RESET } from '../constants/productConstants'
 
 function ProductEditScreen() {
   const productParams = useParams();
@@ -29,24 +30,42 @@ function ProductEditScreen() {
   const productDetails = useSelector((state) => state.productDetails);
   const { error, loading, product } = productDetails;
 
+  const productUpdate = useSelector((state) => state.productUpdate);
+  const {error:errorUpdate, loading:loadingUpdate, success:successUpdate} = productUpdate
+
   const history = useNavigate();
 
   useEffect(() => {
-    if (!product.name || product._id !== Number(productId)) {
-      dispatch(listProductDetails(productId));
+    if (successUpdate) {
+      dispatch({type: PRODUCT_UPDATE_RESET})
+      history("/admin/productlist")
     } else {
-      setName(product.name);
-      setPrice(product.price);
-      setImage(product.image);
-      setBrand(product.brand);
-      setCategory(product.category);
-      setCountInStock(product.countInStock);
-      setDescription(product.description);
+      if (!product.name || product._id !== Number(productId)) {
+        dispatch(listProductDetails(productId));
+      } else {
+        setName(product.name);
+        setPrice(product.price);
+        setImage(product.image);
+        setBrand(product.brand);
+        setCategory(product.category);
+        setCountInStock(product.countInStock);
+        setDescription(product.description);
+      }
     }
   }, [dispatch, product, productId, history]);
 
   const submitHandler = (e) => {
     e.preventDefault();
+    dispatch(updateProduct({
+      _id:productId,
+      name,
+      price,
+      image,
+      brand,
+      category,
+      countInStock,
+      description
+    }))
   };
 
   return (
@@ -54,6 +73,8 @@ function ProductEditScreen() {
       <Link to="/admin/userlist">Go Back</Link>
       <FormContainer>
         <h1>Edit Product</h1>
+        {loadingUpdate && <Loader />}
+        {errorUpdate && <Message variant="danger">{errorUpdate}</Message>}
         {loading ? (
           <Loader></Loader>
         ) : error ? (

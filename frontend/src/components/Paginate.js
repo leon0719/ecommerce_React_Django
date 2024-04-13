2f2008cee4b198c899bf6206f3dd4f58bdaa1102
leon <90852553+leon0719@users.noreply.github.com>
2024-04-13 22:55:46 +0800

    新增 Paginate 但在 Admin products list 有 bug 無法顯示分頁後的資料 ; ;

--- frontend/src/components/Paginate.js
+++ frontend/src/components/Paginate.js
@@ -1,25 +1,35 @@
-import React from 'react'
-import { Pagination } from 'react-bootstrap'
-import { LinkContainer } from 'react-router-bootstrap'
+import React from "react";
+import { Pagination } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 
-function Paginate({ pages, page, keyword = '', isAdmin = false }) {
-
-    return (pages > 1 && (
-        <Pagination>
-            {[...Array(pages).keys()].map((x) => (
-                <LinkContainer
-                    key={x + 1}
-                    to={{
-                        pathname: '/',
-                        search: `?keyword=${keyword}&page=${x + 1}`
-                    }}
-                >
-                    <Pagination.Item active={x + 1 === page}>{x + 1}</Pagination.Item>
-                </LinkContainer>
-            ))}
-        </Pagination>
+function Paginate({ pages, page, keyword = "", isAdmin = false }) {
+  if (!keyword) {
+    keyword = "";
+  }
+  return (
+    pages > 1 && (
+      <Pagination>
+        {[...Array(pages).keys()].map((x) => (
+          <LinkContainer
+            key={x + 1}
+            to={
+              !isAdmin
+                ? {
+                    pathname: "/",
+                    search: `?keyword=${keyword}&page=${x + 1}`,
+                  }
+                : {
+                    pathname: "/admin/productlist/",
+                    search: `?keyword=${keyword}&page=${x + 1}`,
+                  }
+            }
+          >
+            <Pagination.Item active={x + 1 === page}>{x + 1}</Pagination.Item>
+          </LinkContainer>
+        ))}
+      </Pagination>
     )
-    )
+  );
 }
 
-export default Paginate
+export default Paginate;

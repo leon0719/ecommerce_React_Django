d23c73d9f5d3c50b717db591c180a2330cc69dcd
leon <90852553+leon0719@users.noreply.github.com>
2024-04-17 15:46:11 +0800

    新增 carousel constant reducer action

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -6,8 +6,10 @@ import Message from "../components/Message";
 import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { listProducts } from "../actions/productActions";
-import { useSearchParams } from 'react-router-dom';
+import { useSearchParams } from "react-router-dom";
 import Paginate from "../components/Paginate";
+import ProductCarousel from "../components/ProductCarousel";
+
 function HomeScreen() {
   const history = useNavigate();
   const dispatch = useDispatch();
@@ -20,21 +22,22 @@ function HomeScreen() {
   }, [dispatch, searchKeyword]);
   return (
     <div>
+      {!keyword && <ProductCarousel/> }
       <h1>Latest Products</h1>
       {loading ? (
         <Loader></Loader>
       ) : error ? (
-        <Message variant='danger'>{error}</Message>
+        <Message variant="danger">{error}</Message>
       ) : (
         <div>
-        <Row>
-          {products.map((product) => (
-            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-              <Product product={product}></Product>
-            </Col>
-          ))}
-        </Row>
-        <Paginate page={page} pages={pages} keyword={keyword} />
+          <Row>
+            {products.map((product) => (
+              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                <Product product={product}></Product>
+              </Col>
+            ))}
+          </Row>
+          <Paginate page={page} pages={pages} keyword={keyword} />
         </div>
       )}
     </div>
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -11,7 +11,8 @@ import {
   productDeleteReducer,
   productCreateReducer,
   productUpdateReducer,
-  productReviewCreateReducer
+  productReviewCreateReducer,
+  productTopRatedReducer
 } from "./reducers/productReducers";
 import { cartReducer } from "./reducers/cartReducers";
 import {
@@ -39,6 +40,7 @@ const reducer = combineReducers({
   productCreate: productCreateReducer,
   productUpdate: productUpdateReducer,
   productReviewCreate: productReviewCreateReducer,
+  productTopRated: productTopRatedReducer,
 
   cart: cartReducer,
   userLogin: userLoginReducer,

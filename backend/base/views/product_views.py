76889594d9e71287eb470b06adb367cbbe43c1c9
leon <90852553+leon0719@users.noreply.github.com>
2024-04-19 16:28:58 +0800

    update

--- backend/base/views/product_views.py
+++ backend/base/views/product_views.py
@@ -41,6 +41,11 @@ def getProduct(request, pk):
     serializer = ProductSerializer(product, many=False)
     return Response(serializer.data)
 
+@api_view(["GET"])
+def getTopProducts(request):
+    products = Product.objects.filter(rating__gte=4).order_by("-rating")[0:5]
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
 
 @api_view(["POST"])
 @permission_classes([IsAdminUser])
